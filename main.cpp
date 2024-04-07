a85dd2f1964bbdd5897e6d42ac4d79974ff84830
lirongHXY <1751569405@qq.com>
2024-04-07 15:33:02 +0800

    实现分区操作

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namesapce std;
 
+// 分区操作，将数组划分为小于基准元素和大于基准元素的两部分
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low]; // 选择第一个元素作为基准元素
+    int i = low;
+    int j = high;
+    
+    while (i < j) {
+        // 从右向左找到第一个小于基准元素的元素
+        while (arr[j] > pivot) {
+            j--;
+        }
+        // 从左向右找到第一个大于基准元素的元素
+        while (arr[i] <= pivot && i < j) {
+            i++;
+        }
+        // 交换这两个元素
+        if (i < j) {
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    // 将基准元素放到最终位置
+    swap(&arr[low], &arr[j]);
+    return j;
+}
+
 void quicksort(int *arr, int low, int high)
 {
 	if(low < high)
