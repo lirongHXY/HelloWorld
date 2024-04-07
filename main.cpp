f4c11578c7063e69b416ea7a2c6a745c1015b21a
lirongHXY <1751569405@qq.com>
2024-04-07 15:27:51 +0800

    修改排序算法

--- main.cpp
+++ main.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
 using namesapce std;
 
-void sort(int *arr, int size)
+void quicksort(int *arr, int low, int high)
 {
-	for(int i = 0; i < size -1; ++i)
+	if(low < high)
 	{
-		bool isSwap = false;
-		for(int j = 0; j < size -1 - i; ++j)
-		{
-			if(arr[j] > arr[j + 1])
-			{
-				int itemp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = itemp;
-				isSwap = true;
-			}
-		}
-		if(!isSwap)
-		{
-			return;
-		}
+		// 划分数组并获取基准元素的位置
+        int pivot_index = partition(arr, low, high);
+        // 递归地对基准元素左右两侧的子数组进行快速排序
+        quick_sort(arr, low, pivot_index - 1);
+        quick_sort(arr, pivot_index + 1, high);
 	}
 }
 
